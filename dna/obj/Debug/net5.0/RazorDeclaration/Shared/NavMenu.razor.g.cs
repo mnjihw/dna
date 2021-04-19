b4cfc82c4a7993c7831a34e52d9f289e0f2932e7
mnjihw <mnjihw@naver.com>
2021-04-19 23:29:53 +0900

    수정

--- dna/obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
+++ dna/obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
@@ -13,70 +13,70 @@ namespace dna.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "/Users/0eun/Documents/Projects/dna/dna/_Imports.razor"
+#line 1 "C:\Users\mnjihw\Documents\Visual Studio 2019\Projects\dna\dna\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "/Users/0eun/Documents/Projects/dna/dna/_Imports.razor"
+#line 2 "C:\Users\mnjihw\Documents\Visual Studio 2019\Projects\dna\dna\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "/Users/0eun/Documents/Projects/dna/dna/_Imports.razor"
+#line 3 "C:\Users\mnjihw\Documents\Visual Studio 2019\Projects\dna\dna\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "/Users/0eun/Documents/Projects/dna/dna/_Imports.razor"
+#line 4 "C:\Users\mnjihw\Documents\Visual Studio 2019\Projects\dna\dna\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "/Users/0eun/Documents/Projects/dna/dna/_Imports.razor"
+#line 5 "C:\Users\mnjihw\Documents\Visual Studio 2019\Projects\dna\dna\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "/Users/0eun/Documents/Projects/dna/dna/_Imports.razor"
+#line 6 "C:\Users\mnjihw\Documents\Visual Studio 2019\Projects\dna\dna\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "/Users/0eun/Documents/Projects/dna/dna/_Imports.razor"
+#line 7 "C:\Users\mnjihw\Documents\Visual Studio 2019\Projects\dna\dna\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "/Users/0eun/Documents/Projects/dna/dna/_Imports.razor"
+#line 8 "C:\Users\mnjihw\Documents\Visual Studio 2019\Projects\dna\dna\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "/Users/0eun/Documents/Projects/dna/dna/_Imports.razor"
+#line 9 "C:\Users\mnjihw\Documents\Visual Studio 2019\Projects\dna\dna\_Imports.razor"
 using dna;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "/Users/0eun/Documents/Projects/dna/dna/_Imports.razor"
+#line 10 "C:\Users\mnjihw\Documents\Visual Studio 2019\Projects\dna\dna\_Imports.razor"
 using dna.Shared;
 
 #line default
@@ -90,7 +90,7 @@ using dna.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 28 "/Users/0eun/Documents/Projects/dna/dna/Shared/NavMenu.razor"
+#line 28 "C:\Users\mnjihw\Documents\Visual Studio 2019\Projects\dna\dna\Shared\NavMenu.razor"
        
     private bool collapseNavMenu = true;
 
